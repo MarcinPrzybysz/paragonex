de8c92474485e990e8ec834320313e0f00b37f6d
MarcinPrzybysz <prz.marcin@gmail.com>
2020-11-17 22:34:51 +0100

    Date spinner

--- app/src/main/java/pl/przybysz/paragonex/receipt/ReceiptFragment.java
+++ app/src/main/java/pl/przybysz/paragonex/receipt/ReceiptFragment.java
@@ -1,14 +1,19 @@
 package pl.przybysz.paragonex.receipt;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+
+import java.time.LocalDate;
 
 import pl.przybysz.paragonex.ICommunicator;
 import pl.przybysz.paragonex.R;
@@ -23,6 +28,7 @@ public class ReceiptFragment extends Fragment {
     EditText price;
     EditText description;
     Spinner category;
+    DatePicker datePicker;
 
     final String RECEIPT = "paragonex.receipt";
 
@@ -35,39 +41,48 @@ public class ReceiptFragment extends Fragment {
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_add_new, container, false);
+
+        //todo: czy to nie powinno być w on create?
+        View view = inflater.inflate(R.layout.fragment_receipt, container, false);
+        communicator = (ICommunicator) getActivity();
 
         shop = view.findViewById(R.id.tv_shop);
         price = view.findViewById(R.id.tv_price);
         description = view.findViewById(R.id.tv_description);
         addBtn = view.findViewById(R.id.button_add);
+        datePicker = view.findViewById(R.id.datePicker);
 
-        communicator = (ICommunicator) getActivity();
         addBtn.setOnClickListener(view1 -> communicator.passDataToReceiptList(loadToDto()));
-
         if (getArguments() != null) {
             Receipt receipt = getArguments().getParcelable(RECEIPT);
             loadFromDto(receipt);
         }
 
-
         return view;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     private void loadFromDto(Receipt receipt) {
-        shop.setText("" + receipt.getShop());
-        price.setText("" + receipt.getPrice());
-        description.setText("" + receipt.getDescription());
+        shop.setText(receipt.getShop() != null ? receipt.getShop() : "");
+        price.setText(receipt.getPrice() != null ? receipt.getPrice().toString() : "0");
+        description.setText(receipt.getDescription() != null ? receipt.getDescription() : "");
+        if (receipt.getDate() != null) {
+            LocalDate date = receipt.getDate();
+            datePicker.updateDate(date.getYear(), date.getMonth().getValue(), date.getDayOfMonth());
+        }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     private Receipt loadToDto() {
         Receipt receipt = new Receipt();
         receipt.setShop(shop.getText().toString());
         receipt.setDescription(description.getText().toString());
         receipt.setPrice(Double.valueOf(price.getText().toString()));
+        receipt.setDate(LocalDate.of(datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth()));
 
         return receipt;
     }
